72050fd90cb18a7541166a539a84c9346acbf95e
SATYAPRAGNYA-KAR <satyakar2019@gmail.com>
2022-02-28 13:09:08 +0530

    Routing Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Recognitions from './frame5/Recognitions';
-import Teams from './frame5/Teams';
-import SacBooking from './frame5/SacBooking';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LayoutApp from "./pages/Teams_Page/LayoutApp";
+import ClubsApp from "./pages/Clubs_Page/ClubsApp";
+import SacBooking from "./frame5/SacBooking";
+import Recognitions from "./frame5/Recognitions";
+import Teams from "./frame5/Teams";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Recognitions/> */}
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* <Recognitions/>
       <SacBooking />
-      {/* <Teams/> */}
-
-    </div>
+      <Teams/> */}
+          <Route path="/clubs" element={<ClubsApp />} />
+          <Route path="/team" element={<LayoutApp />} />
+          <Route path="/sacbooking" element={<SacBooking />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/recognitions" element={<Recognitions />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
