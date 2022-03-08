34394f25e22734f2b5b431746e71001cd4b20b41
Tamanna <tamannatanwar0613@gmail.com>
2022-03-08 21:09:23 +0530

    Update SacBooking.js

--- src/pages/SacBooking/SacBooking.js
+++ src/pages/SacBooking/SacBooking.js
@@ -19,7 +19,7 @@ function Sac_Booking() {
             eventKey="0"
           >
             <Accordion.Header>Criteria and Conditions</Accordion.Header>
-            <Accordion.Body aria-expanded="false">
+            <Accordion.Body  style={{width:"100%" }} aria-expanded="false">
               <ul>
                 <li> No personal events can be organized in SAC premises.</li>
                 <li>
@@ -66,44 +66,7 @@ function Sac_Booking() {
           >
             <Accordion.Header>Rent Charges</Accordion.Header>
             <Accordion.Body>
-              <ul>
-                <li> No personal events can be organized in SAC premises.</li>
-                <li>
-                  The priority will be given to the BRCA club events followed by
-                  other student events, community events, alumni and outside
-                  respectively.
-                </li>
-                <li>
-                  No food items are allowed in committee room and side lawn.
-                </li>
-                <li>
-                  The process of request is through online form on BRCA website.
-                  Once the booking is blocked and mail is communicated to the
-                  requisitioner, the venue charges should be deposited in RCA
-                  account online and the transaction details should be
-                  communicated within 3 days from date of blocking for the
-                  booking to be confirmed.{" "}
-                </li>
-                <li>
-                  Approval of the HOD for departmental events is required and an
-                  email from the HEAD’s official ID to officebrca@gmail.com
-                  along with the booking request form is required for blocking
-                  the venue.
-                </li>
-                <li>
-                  BRCA office and Dean SA office reserves the right to cancel/
-                  modify the reservations without any prior notice.
-                </li>
-                <li>
-                  Students of IITD will be required to submit their I-Cards when
-                  availing access to the venue.{" "}
-                </li>
-                <li>
-                  Any damage to BRCA properties, including marks on walls,
-                  furniture scratches and holes in the lawns etc. will attract
-                  penalty.{" "}
-                </li>
-              </ul>
+              <iFrame style={{width:"100%"}} src="https://docs.google.com/spreadsheets/d/1YA5-QGzKbfIdqWD7sCIB1eyu4ZFSghvWzMe18BI312Q/pubhtml?gid=0&single=true"></iFrame>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item
@@ -112,44 +75,7 @@ function Sac_Booking() {
           >
             <Accordion.Header>Availability Calender</Accordion.Header>
             <Accordion.Body>
-              <ul>
-                <li> No personal events can be organized in SAC premises.</li>
-                <li>
-                  The priority will be given to the BRCA club events followed by
-                  other student events, community events, alumni and outside
-                  respectively.
-                </li>
-                <li>
-                  No food items are allowed in committee room and side lawn.
-                </li>
-                <li>
-                  The process of request is through online form on BRCA website.
-                  Once the booking is blocked and mail is communicated to the
-                  requisitioner, the venue charges should be deposited in RCA
-                  account online and the transaction details should be
-                  communicated within 3 days from date of blocking for the
-                  booking to be confirmed.{" "}
-                </li>
-                <li>
-                  Approval of the HOD for departmental events is required and an
-                  email from the HEAD’s official ID to officebrca@gmail.com
-                  along with the booking request form is required for blocking
-                  the venue.
-                </li>
-                <li>
-                  BRCA office and Dean SA office reserves the right to cancel/
-                  modify the reservations without any prior notice.
-                </li>
-                <li>
-                  Students of IITD will be required to submit their I-Cards when
-                  availing access to the venue.{" "}
-                </li>
-                <li>
-                  Any damage to BRCA properties, including marks on walls,
-                  furniture scratches and holes in the lawns etc. will attract
-                  penalty.{" "}
-                </li>
-              </ul>
+        <iFrame style={{width:"100%", height:"100%", }} src="https://www.google.com/calendar/embed?showPrint=0&showCalendars=0&height=600&wkst=1&bgcolor=%23FFFFFF&src=officebrca%40gmail.com&color=%231B887A&ctz=Asia%2FCalcutta"></iFrame>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item
@@ -158,44 +84,8 @@ function Sac_Booking() {
           >
             <Accordion.Header>Online Booking Form</Accordion.Header>
             <Accordion.Body>
-              <ul>
-                <li> No personal events can be organized in SAC premises.</li>
-                <li>
-                  The priority will be given to the BRCA club events followed by
-                  other student events, community events, alumni and outside
-                  respectively.
-                </li>
-                <li>
-                  No food items are allowed in committee room and side lawn.
-                </li>
-                <li>
-                  The process of request is through online form on BRCA website.
-                  Once the booking is blocked and mail is communicated to the
-                  requisitioner, the venue charges should be deposited in RCA
-                  account online and the transaction details should be
-                  communicated within 3 days from date of blocking for the
-                  booking to be confirmed.{" "}
-                </li>
-                <li>
-                  Approval of the HOD for departmental events is required and an
-                  email from the HEAD’s official ID to officebrca@gmail.com
-                  along with the booking request form is required for blocking
-                  the venue.
-                </li>
-                <li>
-                  BRCA office and Dean SA office reserves the right to cancel/
-                  modify the reservations without any prior notice.
-                </li>
-                <li>
-                  Students of IITD will be required to submit their I-Cards when
-                  availing access to the venue.{" "}
-                </li>
-                <li>
-                  Any damage to BRCA properties, including marks on walls,
-                  furniture scratches and holes in the lawns etc. will attract
-                  penalty.{" "}
-                </li>
-              </ul>
+            <iFrame style={{width:"100%", height:"100%", }} src="https://docs.google.com/forms/d/e/1FAIpQLSetUlkoZ8LT8acvuVenKZ8bTUJ8IY4QApPhLW9Qip_g862AlQ/viewform?embedded=true"></iFrame>
+            
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item
@@ -204,44 +94,12 @@ function Sac_Booking() {
           >
             <Accordion.Header>Booking Process</Accordion.Header>
             <Accordion.Body>
-              <ul>
-                <li> No personal events can be organized in SAC premises.</li>
-                <li>
-                  The priority will be given to the BRCA club events followed by
-                  other student events, community events, alumni and outside
-                  respectively.
-                </li>
-                <li>
-                  No food items are allowed in committee room and side lawn.
-                </li>
-                <li>
-                  The process of request is through online form on BRCA website.
-                  Once the booking is blocked and mail is communicated to the
-                  requisitioner, the venue charges should be deposited in RCA
-                  account online and the transaction details should be
-                  communicated within 3 days from date of blocking for the
-                  booking to be confirmed.{" "}
-                </li>
-                <li>
-                  Approval of the HOD for departmental events is required and an
-                  email from the HEAD’s official ID to officebrca@gmail.com
-                  along with the booking request form is required for blocking
-                  the venue.
-                </li>
-                <li>
-                  BRCA office and Dean SA office reserves the right to cancel/
-                  modify the reservations without any prior notice.
-                </li>
-                <li>
-                  Students of IITD will be required to submit their I-Cards when
-                  availing access to the venue.{" "}
-                </li>
-                <li>
-                  Any damage to BRCA properties, including marks on walls,
-                  furniture scratches and holes in the lawns etc. will attract
-                  penalty.{" "}
-                </li>
-              </ul>
+              <ol>
+                <li>Send Booking Request Online</li>
+                <li>Receive email of venue booking/rejection within 24 hrs</li>
+                <li>Pay the Rent amount and communicate to SAC Office</li>
+                <li>Receive Confirmation mail</li>
+              </ol>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
